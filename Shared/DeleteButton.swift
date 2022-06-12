cc5a3dc05e9c614ead339ba7ac81bef0e4b9389c
Nick Chong <cwsnick@outlook.com>
2022-06-12 21:44:09 +0800

    add login page and add to readme

---
+++ Shared/DeleteButton.swift
@@ -0,0 +1,20 @@
+//
+//  DeleteButton.swift
+//  base_todoList-swiftUI-core_date
+//
+//  Created by Wan Si Chong on 9/6/2022.
+//
+
+import SwiftUI
+
+struct DeleteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeleteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        DeleteButton()
+    }
+}
